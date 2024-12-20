a69e28de87bc2b371d24f328a4de5cac731f4008
Acecooper247 <acec01876@gmail.com>
2024-12-20 09:32:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const defaultOwner = '2348115316627';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || 'your number';  // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '2348115316627';  // put your number here
 
 const ownerlist = ownervb.split(';');
 
